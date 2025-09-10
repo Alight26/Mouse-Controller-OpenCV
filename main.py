977b376f7b9ffc069d330a217d7d25c65343f92e
Lucas <luc.coulson6@gmail.com>
2025-09-10 17:54:19 -0400

    video and hand detection work

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import cv2 as cv 
 import mediapipe as mp 
+import pyautogui 
+import numpy as np
 
 
 mp_hands = mp.solutions.hands
 mp_draw = mp.solutions.drawing_utils
-hands = mp_hands.Hands(min_detection_confidence= 0.5,
-                    min_tracking_confidence = 0.5)
+hands = mp_hands.Hands(min_detection_confidence= 0.7,
+                    min_tracking_confidence = 0.7)
 
 
 cap = cv.VideoCapture(1)
+
+screen_width, screen_height = pyautogui.size()
+
 
 while True:
     ret, img = cap.read()
@@ -17,7 +22,20 @@ while True:
     if ret is False:
         break
 
+    h, w, c = img.shape
+
     RGB_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    results = hands.process(RGB_img)
+
+    if results.multi_hand_landmarks:
+        for hand_landmarks in results.multi_hand_landmarks:
+
+            # Draws the hand landmarks
+            mp_draw.draw_landmarks(img, hand_landmarks,mp_hands.HAND_CONNECTIONS)
+
+
+
+
 
 
 
